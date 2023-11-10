0fcba4a60dcdfa4411f1116b9d68a2cc666931f3
smile1130 <superskiller1130@gmail.com>
2023-11-10 18:26:00 +0900

    Fix some frontend part

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -41,10 +41,10 @@
                 </div>
             </fieldset>
             <fieldset class="mt-4 flex flex-col gap-2">
-                <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an
-                    option</label>
-                <div date-rangepicker class="flex items-center">
-                    <div class="relative">
+                <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select start and
+                    end date</label>
+                <div date-rangepicker class="flex flex-col items-center">
+                    <div class="relative w-full">
                         <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                             <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
                                 xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
@@ -57,7 +57,7 @@
                             placeholder="Select date start">
                     </div>
                     <span class="mx-4 text-gray-500">to</span>
-                    <div class="relative">
+                    <div class="relative w-full">
                         <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                             <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
                                 xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
@@ -72,8 +72,8 @@
                 </div>
             </fieldset>
             <fieldset>
-                <label for="userlists" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an
-                    option</label>
+                <label for="userlists" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select user
+                    list</label>
                 <select id="userlists"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                     <option selected value="0">Choose a user list</option>
